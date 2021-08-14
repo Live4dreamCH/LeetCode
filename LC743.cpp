6ef4861fd8fca1c85e2d3d352f22cd88d322d0ca
Live4dreamCH <live4dream@stu.xjtu.edu.cn>
2021-08-14 16:45:06 +0800

    Dijkstra

--- LC743.cpp
+++ LC743.cpp
@@ -1,68 +1,68 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
-#include <utility>
-#include <queue>
-#include <list>
+#include <unordered_set>
 using namespace std;
 
 class Solution
 {
 public:
+    // Dijkstra
     int networkDelayTime(vector<vector<int>> &times, int n, int k)
     {
-        vector<int> dis(n + 5, -1);
-        queue<int> edge;
-        vector<list<pair<int, int>>> nei(n + 5);
-        vector<bool> visited(n + 5, false);
-        edge.push(k);
+        // 到k的距离
+        vector<unsigned int> dis(n + 5, -1);
         dis[k] = 0;
-        for (auto &t : times)
+
+        // 邻接表
+        vector<vector<pair<int, int>>> nei(n + 5);
+        for (const auto &t : times)
         {
-            auto &link = nei[t[0]];
-            if (link.empty())
+            nei[t[0]].emplace_back(t[1], t[2]);
+        }
+
+        // 已找到最短距离的点集
+        unordered_set<int> shortest;
+
+        // 是否进入下一循环
+        bool con = true;
+        // 当前节点, 用出边更新dis
+        int curr = k;
+        while (con)
+        {
+            shortest.emplace(curr);
+            con = false;
+            // 更新最短距离
+            for (const auto &edge : nei[curr])
             {
-                link.push_back({t[1], t[2]});
-                continue;
-            }
-            for (auto it = link.begin();; ++it)
-            {
-                if (it->first > t[1] || it == link.end())
+                auto new_dis = dis[curr] + edge.second;
+                if (new_dis < dis[edge.first])
                 {
-                    link.insert(it, {t[1], t[2]});
-                    break;
+                    dis[edge.first] = new_dis;
+                }
+            }
+
+            // 找到下一个最短距离的点
+            unsigned int min_dis = -1;
+            for (int i = 1; i <= n; ++i)
+            {
+                if (shortest.find(i) == shortest.end() && dis[i] < min_dis)
+                {
+                    min_dis = dis[i];
+                    curr = i;
+                    con = true;
                 }
             }
         }
-        while (!edge.empty())
+
+        // 在所有最短距离里找最长距离
+        if (shortest.size() != n)
         {
-            int &curr = edge.front();
-            visited[curr] = true;
-            auto &link = nei[curr];
-            for (auto it = link.begin(); it != link.end(); ++it)
-            {
-                int &target = it->first, &wei = it->second;
-                int temp = wei + dis[curr];
-                if (dis[target] == -1)
-                {
-                    dis[target] = temp;
-                }
-                else
-                {
-                    dis[target] = temp < dis[target] ? temp : dis[target];
-                }
-                if (!visited[target])
-                    edge.push(target);
-            }
-            edge.pop();
+            return -1;
         }
-        int max_len = -1;
+        unsigned int max_len = 0;
         for (int i = 1; i <= n; ++i)
         {
-            if (dis[i] == -1)
-            {
-                return -1;
-            }
+            // 坑: dis[i]是无符号数, 执行无符号数的>, 若max_len初值为-1, 就是无符号数下的ffffffff, 没有比它大的
             if (dis[i] > max_len)
             {
                 max_len = dis[i];
@@ -76,6 +76,6 @@ int main()
 {
     Solution s;
     vector<vector<int>> temp{{2, 1, 1}, {2, 3, 1}, {3, 4, 1}};
-    s.networkDelayTime(temp, 4, 2);
+    cout << s.networkDelayTime(temp, 4, 2);
     return 0;
 }
