3c21401262b5f5a5c6525f0f6f2887c9282f875d
Charlees <live4dreamch@gmail.com>
2025-05-14 22:24:20 +0800

    标记了constexpr函数不仅可以在编译期使用，还可以在运行时使用，由调用位置决定它到底何时运行，注意分辨

--- LC279.cpp
+++ LC279.cpp
@@ -1,8 +1,9 @@
 #include "MyLib.h"
 
 // rm ./LC279.out ; g++ LC279.cpp -std=c++2a -isystem benchmark/include \
-//   -Lbenchmark/build/src -lbenchmark -lpthread -o LC279.out -g -O2 ; ./LC279.out
-// 2025-05-14T18:56:19+08:00
+//   -Lbenchmark/build/src -lbenchmark -lpthread -o LC279.out -g -O2 -fconstexpr-ops-limit=2147000000 ; ./LC279.out
+// rm: cannot remove './LC279.out': No such file or directory
+// 2025-05-14T22:03:25+08:00
 // Running ./LC279.out
 // Run on (16 X 3686.4 MHz CPU s)
 // CPU Caches:
@@ -10,7 +11,7 @@
 //   L1 Instruction 32 KiB (x8)
 //   L2 Unified 1280 KiB (x8)
 //   L3 Unified 20480 KiB (x1)
-// Load Average: 0.55, 0.41, 0.29
+// Load Average: 0.29, 0.27, 0.17
 // i=1, sum=0
 // i=10, sum=19
 // i=100, sum=261
@@ -21,13 +22,13 @@
 // i=10000000, sum=28815000
 // i=100000000, sum=288150000
 // i=1000000000, sum=-1413467296
-// i=1538634001, sum=138606480
+// i=1543052374, sum=151338018
 // -----------------------------------------------------
 // Benchmark           Time             CPU   Iterations
 // -----------------------------------------------------
-// BM_Package      0.447 ns        0.457 ns   1538634001
+// BM_Package      0.443 ns        0.463 ns   1543052374
 constexpr size_t N = 10000;
-std::array<int, N + 1> get_res_array() {
+constexpr std::array<int, N + 1> get_res_array() {
     std::array<int, N + 1> temp{};
     for (int i = 0; i <= N; ++i) {
         temp[i] = i;
@@ -40,7 +41,7 @@ std::array<int, N + 1> get_res_array() {
     }
     return temp;
 }
-std::array<int, N + 1> global_arr = get_res_array();
+constexpr std::array<int, N + 1> global_arr = get_res_array();
 
 class Solution {
    public:
@@ -48,11 +49,7 @@ class Solution {
 };
 
 int main() {
-    int n{};
     Solution s;
-    while (1) {
-        std::cin >> n;
-        std::cout << s.numSquares(n);
-    }
-    return 0;
+
+    return s.numSquares(9999);
 }
