6cf4886a1b6c53090800ceef9be2c7b9959d18eb
Charlees <live4dreamch@gmail.com>
2025-05-14 18:11:03 +0800

    编译期求值

--- LC279.cpp
+++ LC279.cpp
@@ -1,30 +1,22 @@
 #include "MyLib.h"
 
+constexpr size_t N = 10000;
+constexpr std::array<int, N + 1> get_res_array() {
+    std::array<int, N + 1> temp{};
+    for (int i = 0; i <= N; ++i) {
+        temp[i] = i;
+    }
+    for (int i = 2; i * i <= N; ++i) {
+        for (int n = i * i; n <= N; ++n) {
+            temp[n] = std::min(temp[n], temp[n - i * i] + 1);
+        }
+    }
+    return temp;
+}
+
 class Solution {
-   private:
-    vector<int> count_vec = vector<int>(10005, INT_MAX);
-
    public:
-    Solution() {
-        for (int i = 1; i * i < count_vec.size(); ++i) {
-            count_vec[i * i] = 1;
-        }
-    }
-
-    int numSquares(int n) {
-        if (count_vec[n] != INT_MAX) {
-            // std::cout << "read " << n << "=" << count_vec[n] << '\n';
-            return count_vec[n];
-        }
-        for (int i = 1; i <= n / 2; ++i) {
-            int curr = numSquares(i) + numSquares(n - i);
-            if (curr < count_vec[n]) {
-                count_vec[n] = curr;
-            }
-        }
-        std::cout << "calc " << n << "=" << count_vec[n] << '\n';
-        return count_vec[n];
-    }
+    int numSquares(int n) { return get_res_array()[n]; }
 };
 
 int main() {
